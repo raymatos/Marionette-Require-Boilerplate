a6d1d71f6d97ad2c073d9776010c179c1896173f
Raymond Matos <ray.matos@medialets.com>
2013-02-10 17:59:19 -0500

    Added Compass/sass compiler
    Signed-off-by: Raymond Matos <ray.matos@medialets.com>

--- server/server.js
+++ server/server.js
@@ -1,21 +1,60 @@
 // DEPENDENCIES
 // ============
-var express = require("express"),
-    http = require("http"),
+var express = require('express'),
+    http = require('http'),
     port = (process.env.PORT || 8001),
-    fs = require("fs"),
+    fs = require('fs'),
+    path = require('path'),
+    spawn = require('child_process').spawn,
     server = module.exports = express();
+
+
+// SASS CONFIGURATION
+// ====================
+
+var opts = {
+	'mode': 'compress',
+	'comments': false,
+	'relative': true,
+	'project': path.join(process.cwd(), 'public')
+};
+
+var compass = spawn(
+  'compass',
+  [
+    'compile',
+    opts.comments ? '' : '--no-line-comments',
+    opts.relative ? '--relative-assets' : '',
+    '-s', opts.mode
+  ],
+  {
+    cwd: opts.project
+  }
+);
+
+compass.stdout.setEncoding('utf8');
+	compass.stdout.on('data', function (data) {
+    console.log(data);
+});
+
+compass.on('exit', function (code) {
+    if (code !== 0) {
+        console.log('EEROR COMPASS COMPILE CSS');
+    }
+});
+
+
 
 // SERVER CONFIGURATION
 // ====================
 server.configure(function () {
     
-    server.use("/css", express["static"](__dirname + "/../public/css"));
-	server.use("/img", express["static"](__dirname + "/../public/img"));
-	server.use("/js", express["static"](__dirname + "/../public/js"));
+    server.use('/css', express['static'](__dirname + '/../public/css'));
+	server.use('/img', express['static'](__dirname + '/../public/img'));
+	server.use('/js', express['static'](__dirname + '/../public/js'));
 	
-	server.get("*", function(req, res) {
-	  	fs.createReadStream(__dirname + "/../public/index.html").pipe(res);
+	server.get('*', function(req, res) {
+	  	fs.createReadStream(__dirname + '/../public/index.html').pipe(res);
 	});
     
     server.use(express.errorHandler({
